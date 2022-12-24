ugurhangul/BulkUpgrade

Initial commit.

BulkRebuild/Properties/AssemblyInfo.cs
@@ -1,0 +1,39 @@
+﻿using BulkRebuild;
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BulkRebuild")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BulkRebuild")]
+[assembly: AssemblyCopyright("Copyright © Cabadam 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("611bfd3e-fb6b-47be-9f68-d1f6ae76ffdc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: MelonInfo(typeof(BulkRebuildMod), "BulkRebuild", "0.1", "Cabadam")] //MelonInfo(typeof(ClassOfMod), “Game Mod Name”, “Version”, “Author”)
+[assembly: MelonGame("Crate Entertainment", "Farthest Frontier")] //MelonGame(“Studio”, “Game”)]

cabadam/HunterRetreat

HunterRetreat/Properties/AssemblyInfo.cs
@@ -1,0 +1,39 @@
+﻿using HunterRetreat;
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HunterRetreat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HunterRetreat")]
+[assembly: AssemblyCopyright("Copyright © Cabadam 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0E535424-0769-4B81-A23B-4B11D9B1FE1E")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: MelonInfo(typeof(HunterRetreatMod), "HunterRetreat", "0.1", "Cabadam")] //MelonInfo(typeof(ClassOfMod), “Game Mod Name”, “Version”, “Author”)
+[assembly: MelonGame("Crate Entertainment", "Farthest Frontier")] //MelonGame(“Studio”, “Game”)]